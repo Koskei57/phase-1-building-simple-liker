f7b3d1886993287c16290ef6f652bf864c123ef6
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-16 15:19:56 +0300

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,22 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+function micServe(event) {
+    const heart = event.target;
+    mimicServerCall("url")
+        .then(response => {
+            if (heart.innerText === EMPTY_HEART) {
+                heart.innerText = FULL_HEART;
+                heart.className = "activated-heart";
+            } else {
+                heart.innerText = EMPTY_HEART;
+                heart.className = "";
+            }
+        })
+
+}
 
 
 
@@ -11,15 +27,15 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            let isRandomFailure = Math.random() < .2
+            if (isRandomFailure) {
+                reject("Random server error. Try again.");
+            } else {
+                resolve("Pretend remote server notified of action!");
+            }
+        }, 300);
+    });
 }
